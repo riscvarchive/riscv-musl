4a78454dfdfa905a715fd1a7bdd4d78db922663b
Aric Belsito <lluixhi@gmail.com>
2016-01-06 16:30:28 -0800

    Implement atomic_base lazily. * Need to verify that it works. * Change some casts in atomic_a.

--- arch/riscv32/atomic_base.h
+++ arch/riscv32/atomic_base.h
@@ -1,61 +1,97 @@
-static inline void a_aq()
+static inline void __a_aq()
 {
         __asm__("fence r, rw");
 }
 
-static inline void a_rl()
+static inline void __a_rl()
 {
         __asm__("fence rw, w");
 }
 
-static inline void *a_cas_p(volatile void *p, void *t, void *s)
-{
-        return NULL;
-}
-
 static inline int a_cas(volatile int *p, int t, int s)
 {
-        return NULL;
+        __a_aq();
+        int old = *p;
+        if (*p == t) {
+                *p = s;
+        }
+        __a_rl();
+        return t;
+}
+
+static inline void *a_cas_p(volatile void *p, void *t, void *s)
+{
+        return (void *) a_cas(p, (int) t, (int) s);
 }
 
 static inline int a_swap(volatile int *x, int v)
 {
-        return NULL;
+        __a_aq();
+        int old = *x;
+        *x = v;
+        __a_rl();
+        return old;
 }
 
 static inline int a_fetch_add(volatile int *x, int v)
 {
-        return NULL;
+        __a_aq();
+        int tmp = *x + v;
+        *x = tmp;
+        __a_rl();
+        return tmp;
 }
 
 static inline void a_inc(volatile int *x)
 {
+        __a_aq();
+        *x = *x + 1;
+        __a_rl();
 }
 
 static inline void a_dec(volatile int *x)
 {
+        __a_aq();
+        *x = *x - 1;
+        __a_rl();
 }
 
 static inline void a_and_64(volatile uint64_t *p, uint64_t v)
 {
+        __a_aq();
+        *p &= v;
+        __a_rl();
 }
 
 static inline void a_and(volatile int *p, int v)
 {
+        __a_aq();
+        *p &= v;
+        __a_rl();
 }
 
 static inline void a_or_64(volatile uint64_t *p, uint64_t v)
 {
-}
-
-static inline void a_or_l(volatile void *p, long v)
-{
+        __a_aq();
+        *p |= v;
+        __a_rl();
 }
 
 static inline void a_or(volatile int *p, int v)
 {
+        __a_aq();
+        *p |= v;
+        __a_rl();
+}
+
+static inline void a_or_l(volatile void *p, long v)
+{
+        a_or(p, (int) v);
 }
 
 static inline void a_store(volatile int *p, int x)
 {
+        __a_aq();
+        *p = x;
+        __a_rl();
 }
