eed337f77602122eff27bcc028a84c2ca9da84ea
Aric Belsito <lluixhi@gmail.com>
2016-01-10 18:39:10 -0800

    Fixups, completion of a couple more arch headers. * no shared library support yet. * pthread_arch and syscall_arch are semi-complete.

---
+++ arch/riscv32/pthread_arch.h
@@ -0,0 +1,10 @@
+static inline struct pthread *__pthread_self()
+{
+        struct pthread *self;
+        __asm__ __volatile__("addi %0, tp, 0" : "=r"(self));
+        return self;
+}
+
+#define TP_ADJ(p) (p)
+
+#define MC_PC gregs[REG_PC]
