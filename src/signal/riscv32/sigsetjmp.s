8459bc80558004693a4c5f5d5cd26f772e04ccbc
Aric Belsito <lluixhi@gmail.com>
2016-01-12 22:52:10 -0800

    Add sigsetjmp.

---
+++ src/signal/riscv32/sigsetjmp.s
@@ -0,0 +1,21 @@
+.global sigsetjmp
+.global __sigsetjmp
+.type sigsetjmp, %function
+.type __sigsetjmp, %function
+sigsetjmp:
+__sigsetjmp:
+        bez a1, setjmp
+
+        sw ra, 164(a0)
+        sw s0, 168(a0)
+        mv s0, a0
+
+        jal setjmp
+
+        mv a1, a0
+        mv a0, s0
+        lw ra, 164(a0)
+        lw s0, 168(a0)
+
+.hidden __sigsetjmp_tail
+        j __sigsetjmp_tail
---
+++ src/signal/riscv64/sigsetjmp.s
@@ -0,0 +1,21 @@
+.global sigsetjmp
+.global __sigsetjmp
+.type sigsetjmp, %function
+.type __sigsetjmp, %function
+sigsetjmp:
+__sigsetjmp:
+        bez a1, setjmp
+
+        sd ra, 224(a0)
+        sd s0, 232(a0)
+        mv s0, a0
+
+        jal setjmp
+
+        mv a1, a0
+        mv a0, s0
+        ld ra, 224(a0)
+        ld s0, 232(a0)
+
+.hidden __sigsetjmp_tail
+        j __sigsetjmp_tail
