81da372bfc37ec0f0c46676f07154cd9ae48b3c8
Alex Suykov <alex.suykov@gmail.com>
2016-10-20 00:49:32 +0300

    undo SYS_llseek patch, use __llseek throughout

--- src/stdio/__stdio_seek.c
+++ src/stdio/__stdio_seek.c
@@ -1,13 +1,9 @@
 #include "stdio_impl.h"
 
-#if defined(SYS__llseek) && !defined(SYS_llseek)
-#define SYS_llseek SYS__llseek
-#endif
-
 off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
 	off_t ret;
-#ifdef SYS_llseek
+#ifdef SYS__llseek
 	if (syscall(SYS_llseek, f->fd, off>>32, off, &ret, whence)<0)
 		ret = -1;
 #else
--- src/unistd/lseek.c
+++ src/unistd/lseek.c
@@ -2,13 +2,9 @@
 #include "syscall.h"
 #include "libc.h"
 
-#if defined(SYS__llseek) && !defined(SYS_llseek)
-#define SYS_llseek SYS__llseek
-#endif
-
 off_t lseek(int fd, off_t offset, int whence)
 {
-#ifdef SYS_llseek
+#ifdef SYS__llseek
 	off_t result;
 	return syscall(SYS_llseek, fd, offset>>32, offset, &result, whence) ? -1 : result;
 #else
