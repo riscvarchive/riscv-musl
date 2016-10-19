05a3ca0ef211c1bccd13e7bbce4f7accc68dbd86
Alex Suykov <alex.suykov@gmail.com>
2016-10-19 03:09:33 +0300

    handle both SYS__llseek and SYS_llseek definitions

--- src/stdio/__stdio_seek.c
+++ src/stdio/__stdio_seek.c
@@ -1,10 +1,14 @@
 #include "stdio_impl.h"
 
+#if defined(SYS__llseek) && !defined(SYS_llseek)
+#define SYS_llseek SYS__llseek
+#endif
+
 off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
 	off_t ret;
-#ifdef SYS__llseek
-	if (syscall(SYS__llseek, f->fd, off>>32, off, &ret, whence)<0)
+#ifdef SYS_llseek
+	if (syscall(SYS_llseek, f->fd, off>>32, off, &ret, whence)<0)
 		ret = -1;
 #else
 	ret = syscall(SYS_lseek, f->fd, off, whence);
--- src/unistd/lseek.c
+++ src/unistd/lseek.c
@@ -2,11 +2,15 @@
 #include "syscall.h"
 #include "libc.h"
 
+#if defined(SYS__llseek) && !defined(SYS_llseek)
+#define SYS_llseek SYS__llseek
+#endif
+
 off_t lseek(int fd, off_t offset, int whence)
 {
-#ifdef SYS__llseek
+#ifdef SYS_llseek
 	off_t result;
-	return syscall(SYS__llseek, fd, offset>>32, offset, &result, whence) ? -1 : result;
+	return syscall(SYS_llseek, fd, offset>>32, offset, &result, whence) ? -1 : result;
 #else
 	return syscall(SYS_lseek, fd, offset, whence);
 #endif
