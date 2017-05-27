9e2f68b3db224f539190bc5f61b58cb07b13d6ab
Michael Clark <michaeljclark@mac.com>
2017-05-27 20:07:44 +1200

    Update to use asm-generic stat.h

--- arch/riscv64/bits/stat.h
+++ arch/riscv64/bits/stat.h
@@ -1,23 +1,18 @@
-/* copied from kernel definition, but with padding replaced
- * by the corresponding correctly-sized userspace types. */
-
-struct stat
-{
+struct stat {
 	dev_t st_dev;
 	ino_t st_ino;
-	nlink_t st_nlink;
-
 	mode_t st_mode;
+	nlink_t st_nlink;
 	uid_t st_uid;
 	gid_t st_gid;
-	unsigned int    __pad0;
 	dev_t st_rdev;
+	unsigned long __pad;
 	off_t st_size;
 	blksize_t st_blksize;
+	int __pad2;
 	blkcnt_t st_blocks;
-
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
-	long __unused[3];
+	unsigned __unused[2];
 };
