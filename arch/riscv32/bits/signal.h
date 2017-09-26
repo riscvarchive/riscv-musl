41c5ff87689eaf8d62b0276add4f71162d6abdc3
Michael Clark <michaeljclark@mac.com>
2017-09-27 12:05:33 +1300

    Update riscv gregset_t, fpregset_t and mcontext_t

--- arch/riscv32/bits/signal.h
+++ arch/riscv32/bits/signal.h
@@ -6,30 +6,27 @@
 # define SIGSTKSZ 8192
 #endif
 
-#if (defined(_GNU_SOURCE) || defined(_BSD_SOURCE)) && !defined(IN_GCC)
-typedef unsigned long greg_t, gregset_t[32];
-typedef struct user_fpregs_struct {
-        double f[32];
-        unsigned long fcsr;
+/* gregs[0] holds the program counter. */
+
+#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+typedef unsigned long greg_t;
+typedef unsigned long gregset_t[32];
+
+typedef struct {
+        unsigned long long f[64] __attribute__((aligned(16)));
+        unsigned fcsr;
+        unsigned reserved[3];
 } fpregset_t;
+
 typedef struct sigcontext {
-        struct user_regs_struct {
-                unsigned long pc;
-                unsigned long ra;
-                unsigned long sp;
-                unsigned long gp;
-                unsigned long tp;
-                unsigned long t0, t1, t2;
-                unsigned long s0, s1;
-                unsigned long a0, a1, a2, a3, a4, a5, a6, a7;
-                unsigned long s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
-                unsigned long t3, t4, t5, t6;
-        } sc_regs;
-        struct user_fpregs_struct sc_fpregs;
+        gregset_t gregs;
+        fpregset_t fpregs;
 } mcontext_t;
+
 #else
-typedef struct sigcontext {
-        unsigned long gregs[32 + 2*32 + 1];
+typedef struct {
+        unsigned long gregs[32];
+        unsigned long long fpregs[66] __attribute__((aligned(16)));
 } mcontext_t;
 #endif
 
