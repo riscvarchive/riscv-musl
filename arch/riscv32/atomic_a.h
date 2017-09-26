65b402375fc243cf4445dd3c24f4b020122fddaa
Michael Clark <michaeljclark@mac.com>
2017-09-26 14:28:22 +1300

    Define minimal working atomics: a_barrier, a_ll, a_sc and a_cas

--- arch/riscv32/atomic_a.h
+++ arch/riscv32/atomic_a.h
@@ -1,55 +1,35 @@
+#define a_barrier a_barrier
+static inline void a_barrier()
+{
+	__asm__ __volatile__ ("fence rw,rw" : : : "memory");
+}
+
+#define a_ll a_ll
+static inline int a_ll(volatile int *p)
+{
+	int v;
+	__asm__ __volatile__ ("lr.w %0, (%1)" : "=&r"(v) : "r"(p));
+	return v;
+}
+
+#define a_sc a_sc
+static inline int a_sc(volatile int *p, int v)
+{
+	int r;
+	__asm__ __volatile__ ("sc.w %0, %2, (%1)" : "=&r"(r) : "r"(p), "r"(v) : "memory");
+	return !r;
+}
+
 #define a_cas a_cas
 static inline int a_cas(volatile int *p, int t, int s)
 {
-        int old, tmp;
-        __asm__("li %1, 1\n"
-                "1:\n\t"
-                "lr.w %0, %2\n\t"
-                "bne %0, %3, 1f\n\t"
-                "sc.w %1, %4, %2\n\t"
-                "bnez %1, 1b\n"
-                "1:"
-                : "=rJ"(old), "+r"(tmp), "+A"(*p)
-                : "r"(t), "r"(s));
-        return old;
-}
-
-#define a_swap a_swap
-static inline int a_swap(volatile int *p, int v)
-{
-        int old;
-        __asm__("amoswap.w %0, %2, %1"
-                : "=rJ"(old), "+A"(*p)
-                : "r"(v));
-        return old;
-}
-
-#define a_fetch_add a_fetch_add
-static inline int a_fetch_add(volatile int *p, int v)
-{
-        int tmp;
-        __asm__("amoadd.w %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
-        return tmp;
-}
-
-#define a_fetch_and a_fetch_and
-static inline int a_fetch_and(volatile int *p, int v)
-{
-        int old;
-        __asm__("amoand.w %0, %2, %1"
-                : "=rJ"(old), "+A"(*p)
-                : "r"(v));
-        return old;
-}
-
-#define a_fetch_or a_fetch_or
-static inline int a_fetch_or(volatile int *p, int v)
-{
-        int old;
-        __asm__("amoor.w %0, %2, %1"
-                : "=rJ"(old), "+A"(*p)
-                : "r"(v));
-        return old;
+	int old, tmp;
+	__asm__("1:  lr.w    %0, %2      \n"
+		"    bne     %0, %3, 1f  \n"
+		"    sc.w    %1, %4, %2  \n"
+		"    bnez    %1, 1b      \n"
+		"1:                      \n"
+		: "=&r"(old), "+r"(tmp), "+A"(*p)
+		: "r"(t), "r"(s));
+	return old;
 }
--- arch/riscv64/atomic_a.h
+++ arch/riscv64/atomic_a.h
@@ -1,91 +1,66 @@
+#define a_barrier a_barrier
+static inline void a_barrier()
+{
+	__asm__ __volatile__ ("fence rw,rw" : : : "memory");
+}
+
+#define a_ll a_ll
+static inline int a_ll(volatile int *p)
+{
+	int v;
+	__asm__ __volatile__ ("lr.w %0, %1" : "=&r"(v), "+A"(*p));
+	return v;
+}
+
+#define a_sc a_sc
+static inline int a_sc(volatile int *p, int v)
+{
+	int r;
+	__asm__ __volatile__ ("sc.w %0, %2, %1" : "=&r"(r), "+A"(*p) : "r"(v) : "memory");
+return !r;
+}
+
 #define a_cas a_cas
 static inline int a_cas(volatile int *p, int t, int s)
 {
-        int old, tmp;
-        __asm__("li %1, 1\n"
-                "1:\n\t"
-                "lr.w %0, %2\n\t"
-                "bne %0, %3, 1f\n\t"
-                "sc.w %1, %4, %2\n\t"
-                "bnez %1, 1b\n"
-                "1:"
-                : "=rJ"(old), "+r"(tmp), "+A"(*p)
-                : "r"(t), "r"(s));
-        return old;
+	int old, tmp;
+	__asm__("1:  lr.w    %0, %2      \n"
+		"    bne     %0, %3, 1f  \n"
+		"    sc.w    %1, %4, %2  \n"
+		"    bnez    %1, 1b      \n"
+		"1:                      \n"
+		: "=&r"(old), "+r"(tmp), "+A"(*p)
+		: "r"(t), "r"(s));
+	return old;
 }
 
-#define a_swap a_swap
-static inline int a_swap(volatile int *p, int v)
+#define a_ll_p a_ll_p
+static inline void *a_ll_p(volatile void *p)
 {
-        int old;
-        __asm__("amoswap.w %0, %2, %1"
-                : "=rJ"(old), "+A"(*p)
-                : "r"(v));
-        return old;
+	void *v;
+	__asm__ __volatile__ ("lr.d %0, %1" : "=&r"(v), "+A"(*(long *)p));
+	return v;
 }
 
-#define a_fetch_add a_fetch_add
-static inline int a_fetch_add(volatile int *p, int v)
+#define a_sc_p a_sc_p
+static inline int a_sc_p(volatile int *p, void *v)
 {
-        int tmp;
-        __asm__("amoadd.w %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
-        return tmp;
-}
-
-#define a_fetch_and a_fetch_and
-static inline int a_fetch_and(volatile int *p, int v)
-{
-        int tmp;
-        __asm__("amoand.w %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
-        return tmp;
-}
-
-#define a_fetch_or a_fetch_or
-static inline int a_fetch_or(volatile int *p, int v)
-{
-        int tmp;
-        __asm__("amoor.w %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
-        return tmp;
-}
-
-#define a_and_64 a_and_64
-static inline void a_and_64(volatile uint64_t *p, uint64_t v)
-{
-        int tmp;
-        __asm__("amoand.d %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
-}
-
-#define a_or_64 a_or_64
-static inline void a_or_64(volatile uint64_t *p, uint64_t v)
-{
-        int tmp;
-        __asm__("amoor.d %0, %2, %1"
-                :"=rJ"(tmp), "+A"(*p)
-                :"r"(v));
+	int r;
+	__asm__ __volatile__ ("sc.d %0, %2, %1" : "=&r"(r), "+A"(*(long *)p) : "r"(v) : "memory");
+	return !r;
 }
 
 #define a_cas_p a_cas_p
 static inline void *a_cas_p(volatile void *p, void *t, void *s)
 {
-        void *old;
-        int tmp;
-        __asm__("li %1, 1\n"
-                "1:\n\t"
-                "lr.d %0, %2\n\t"
-                "bne %0, %3, 1f\n\t"
-                "sc.d %1, %4, %2\n\t"
-                "bnez %1, 1b\n"
-                "1:"
-                : "=rJ"(old), "+r"(tmp), "+A"(*(long *) p)
-                : "r"(t), "r"(s));
-        return old;
+	void *old;
+	int tmp;
+	__asm__("1:  lr.d    %0, %2      \n"
+		"    bne     %0, %3, 1f  \n"
+		"    sc.d    %1, %4, %2  \n"
+		"    bnez    %1, 1b      \n"
+		"1:                      \n"
+		: "=&r"(old), "+r"(tmp), "+A"(*(long *)p)
+		: "r"(t), "r"(s));
+	return old;
 }
-
