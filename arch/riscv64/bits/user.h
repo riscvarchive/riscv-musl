fdd4a0c6deb4ab7a722d7169455d715822af620a
Michael Clark <michaeljclark@mac.com>
2017-11-09 08:22:10 +1300

    Make user_regs_struct and user_fpregs_struct consistent with glibc

--- arch/riscv64/bits/user.h
+++ arch/riscv64/bits/user.h
@@ -1,20 +1,43 @@
-typedef struct user_regs_struct {
-        unsigned long pc;
-        unsigned long ra;
-        unsigned long sp;
-        unsigned long gp;
+struct user_regs_struct {
+	unsigned long pc;
+	unsigned long ra;
+	unsigned long sp;
+	unsigned long gp;
 	unsigned long tp;
-	unsigned long t0, t1, t2;
-	unsigned long s0, s1;
-	unsigned long a0, a1, a2, a3, a4, a5, a6, a7;
-	unsigned long s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
-	unsigned long t3, t4, t5, t6;
-} elf_gregset_t;
+	unsigned long t0;
+	unsigned long t1;
+	unsigned long t2;
+	unsigned long s0;
+	unsigned long s1;
+	unsigned long a0;
+	unsigned long a1;
+	unsigned long a2;
+	unsigned long a3;
+	unsigned long a4;
+	unsigned long a5;
+	unsigned long a6;
+	unsigned long a7;
+	unsigned long s2;
+	unsigned long s3;
+	unsigned long s4;
+	unsigned long s5;
+	unsigned long s6;
+	unsigned long s7;
+	unsigned long s8;
+	unsigned long s9;
+	unsigned long s10;
+	unsigned long s11;
+	unsigned long t3;
+	unsigned long t4;
+	unsigned long t5;
+	unsigned long t6;
+};
+
+struct user_fpregs_struct {
+        double f[32];
+        unsigned int fcsr;
+};
 
 #define ELF_NGREG 32
-typedef struct user_fpregs_struct {
-        double f[32];
-        unsigned long fcsr;
-} elf_fpregset_t;
-
-typedef unsigned long elf_greg_t;
+typedef unsigned long elf_greg_t, elf_gregset_t[ELF_NGREG];
+typedef struct user_fpregs_struct elf_fpregset_t;
