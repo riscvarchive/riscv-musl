cd9568153a72e199bb34e17d2f6fe902f11ac646
Drew DeVault <sir@cmpwn.com>
2018-12-08 22:13:38 -0500

    RISC-V: Use v2 syscalls when appropriate
    The RISC-V port does not support any legacy syscalls.

--- src/stdio/rename.c
+++ src/stdio/rename.c
@@ -7,6 +7,10 @@ int rename(const char *old, const char *new)
 #ifdef SYS_rename
 	return syscall(SYS_rename, old, new);
 #else
+#ifdef SYS_renameat2
+	return syscall(SYS_renameat2, AT_FDCWD, old, AT_FDCWD, new, 0);
+#else
 	return syscall(SYS_renameat, AT_FDCWD, old, AT_FDCWD, new);
+#endif
 #endif
 }
